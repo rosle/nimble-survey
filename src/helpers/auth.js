a943007b1e9730a876282175e7a428eb893f5ca6
Rossukhon Leagmongkol <rose.roko@gmail.com>
2021-03-01 11:52:24 +0700

    [Feature] Pin dependency version

--- src/helpers/auth.js
+++ src/helpers/auth.js
@@ -1,9 +1,8 @@
-import AuthApi from 'services/api/auth';
+import AuthApi from 'adapters/api/auth';
 
 const Auth = () => {
   const login = async (email, password) => {
     try {
-      console.log(password);
       await AuthApi.login({ email, password });
       // TODO: Store tokens and redirect to survey list
       return true;
